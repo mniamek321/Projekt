cbb41d4bb3fa061c61ad06a85154f668c2c8336d
Bartosz <99137915+mniamek321@users.noreply.github.com>
2024-01-22 10:04:03 +0100

    Update app.component.ts

--- projekt/src/app/app.component.ts
+++ projekt/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import { NgModule } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+onSubmit() {
+throw new Error('Method not implemented.');
+}
   title = 'projekt';
 }
+
